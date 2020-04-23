2c84f3b43c61afb62c402438f59f6839cbaab5dc
austindickey <austindickey@me.com>
2020-04-23 15:12:05 -0500

    updating server file

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-var express = require("express")
+const express = require("express")
 
-var app = express()
-var port = process.env.PORT || 8000
+const app = express()
+const port = process.env.PORT || 8000
 
+// Middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// Routes
 require("./app/routing/htmlRoutes.js")(app)
 
+// Mongoose
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+mongoose.connect(MONGODB_URI)
+
+// Server Listener
 app.listen(port, function() {
     console.log("Server listening on port: " + port)
 })
